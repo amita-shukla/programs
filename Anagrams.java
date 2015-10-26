dc15d89de698dfe6cc65ee8420cd4f342a743e8b
Amita Shukla <amitashukla0906@gmail.com>
2015-10-26 12:59:26 +0530

    Hashing notes

--- Anagrams.java
+++ Anagrams.java
@@ -13,7 +13,7 @@ dog and god are another set of anagrams which correspond to index 2 and 3.
 The indices are 1 based ( the first element has index 1 instead of index 0).
 
 Ordering of the result : You should not change the relative ordering of the words / phrases within the group. 
-Within a group containing A[i] and A[j], A[i] comes before A[j] if i < j. 
+Within a group containing A[i] and A[j], A[i] comes before A[j] if i < j.
  
 Note: 
 	1. You do not need to use LinkedHashMap here. It is because the relative ordering 'within' each group is considered.

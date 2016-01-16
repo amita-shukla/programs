2246427e29e9d2f42b2e075b1574bc5cd4f4f022
Amita <amitashukla0906@gmail.com>
2016-01-16 17:01:21 +0530

    min time for each mice to fill each hole

--- MiceAndHoles.java
+++ MiceAndHoles.java
@@ -1,7 +1,16 @@
 /**
- *  Sort the mice_position array m[].
-    Sort the hole_position array h[].
-    find the maximum of |m[i] - h[i]|.
+ *  
+	There are N Mice and N holes are placed in a straight line. 
+	Each hole can accomodate only 1 mouse. 
+	A mouse can stay at his position, move one step right from x to x + 1, 
+	or move one step left from x to x − 1. 
+	Any of these moves consumes 1 minute.
+	Assign mice to holes so that the time when the last mouse gets inside a hole is minimized.
+ 
+ 	1. Sort the mice_position array m[].
+    2. Sort the hole_position array h[].
+    3. find the maximum of |m[i] - h[i]|.
+    
  **/
 public class MiceAndHoles {
     public int mice(ArrayList<Integer> a, ArrayList<Integer> b) {

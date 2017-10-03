e45c557d14a1b37d195cf19a595e1c476563252e
amita-shukla <amitashukla0906@gmail.com>
2017-10-03 10:55:53 +0530

    reverse a list problems

--- ReverseList.c
+++ ReverseList.c
@@ -24,6 +24,23 @@ listnode* reverseList(listnode* A) {
     return A;
 }
 
+/**
+Another, shorter solution:
+1->2->3->4->5->NULL
+At any point, 
+3->2->1->4->5 : make node '4' as head, as node '1' (temp) keeps moving towards the end
+**/
+listnode* reverseList(listnode* head) {
+	listnode* temp = head;
+    while(temp->next!=NULL){
+        listnode* next = temp->next;
+        temp->next = next->next;
+        next->next = head;
+        head = next;
+    }
+    return head;
+}
+
 //There is a recursive approach as well
 /**
  * @input A : Head pointer of linked list 
@@ -48,7 +65,7 @@ listnode* reverseList(listnode* A) {
 
 //Reverse a list from m to n:
 listnode* reverseBetween(listnode* head, int m, int n) {
-     listnode* newHead = listnode_new(-1);
+    listnode* newHead = listnode_new(-1);
     newHead->next = head;
     listnode* prev = newHead;
     int i;
@@ -66,6 +83,40 @@ listnode* reverseBetween(listnode* head, int m, int n) {
             cur = prev->next;
         }
     return newHead->next;
+}
+
+/**
+Same logic as the reverseList method, 
+but have to maintain the pointer (temp) next to which the list has to be reversed
+e.g. 1->2->3->4->5->null, m=2,n=4,
+then temp=1(node at m-1), temp2=2(node at m), diff=no of times the reverse has to take place
+catch: what if m=1? then, temp=null and temp2=head  have to manage this case separately.
+**/
+public ListNode reverseBetween(ListNode head, int m, int n) {
+   ListNode temp=null;
+   int count=m;
+   //temp points to the node next from which the reversing starts
+   while(count-->1){
+       if(temp==null) temp=head; 
+       else temp=temp.next;
+   }
+
+   int diff = n-m;
+   ListNode temp2;
+   if(temp==null) temp2=head; 
+   else temp2=temp.next;
+   while(diff-->0){
+       ListNode next = temp2.next;
+       temp2.next=next.next;
+       if(temp==null){
+           next.next=head;
+           head=next;
+       }else{
+           next.next=temp.next;
+           temp.next=next;
+       }
+   }
+   return head;
 }
 
 
